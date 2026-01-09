a0ba5247ce15238b8db4e2de23282f7f9ac34e86
ramaa007 <info@squareconnectgroup.com>
2026-01-10 09:33:12 +1000

    Manually added tailwind and postcss configs

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#DFA528",
+        charcoal: "#383838",
+        appleGrey: "#F5F5F7",
+      },
+    },
+  },
+  plugins: [],
+}
